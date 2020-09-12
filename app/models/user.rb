9ce278b1b09e217a3f24030dd4c0f4ccf743dc0a
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-09-12 13:43:23 +0900

    新規登録実装

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  regexp_name = /\A[ぁ-んァ-ン一-龥]/
+  regexp_name_kana = /\A[ァ-ヶー－]+\z/
+  regexp_password = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,100}+\z/
+
+  with_options presence: true do
+    validates :nickname, :birthday
+    validates :email, uniqueness: true
+    validates :first_name, :last_name, format: { with: regexp_name }
+    validates :first_name_read, :last_name_read, format: { with: regexp_name_kana }
+    validates :password, format: { with: regexp_password }
+  end
+end
