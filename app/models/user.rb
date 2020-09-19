5c509c11223e3bbd2b2af0e289236933f1dbef39
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-09-19 21:48:46 +0900

    ニックネーム変更機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
   has_many :chefs
 
   regexp_name = /\A[ぁ-んァ-ン一-龥]/
-  regexp_name_kana = /\A[ァ-ヶー－]+\z/
+  regexp_name_read = /\A[ァ-ヶー－]+\z/
   regexp_password = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,100}+\z/
 
   with_options presence: true do
     validates :nickname, :birthday
     validates :email, uniqueness: true
     validates :first_name, :last_name, format: { with: regexp_name }
-    validates :first_name_read, :last_name_read, format: { with: regexp_name_kana }
-    validates :password, format: { with: regexp_password }
+    validates :first_name_read, :last_name_read, format: { with: regexp_name_read }
+    validates :password, format: { with: regexp_password }, on: :create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   root to: "chefs#index"
+
+  resources :users, only: [:show, :update]
 
   resources :chefs, only:[:index, :new, :create, :show, :destroy] do
     resources :courses, only:[:new, :create, :edit, :destroy, :update]
