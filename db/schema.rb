10b0b98e0a47c9ab4e6903bf00efb38328cd5b00
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-09-21 19:16:33 +0900

    クレカ登録機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_022021) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "card_token", null: false
-    t.string "cutomer_token", null: false
+    t.string "customer_token", null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
