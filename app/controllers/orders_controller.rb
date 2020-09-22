15bf4268facf9adabd6d96197780ce4dcaf4638a
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-09-22 12:17:13 +0900

    クレカ未登録時予約不可

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,13 @@
 class OrdersController < ApplicationController
   def new
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: current_user.id)
+
+    redirect_to new_card_path and return unless card.present?
+
+    customer = Payjp::Customer.retrieve(card.customer_token)
+    @card = customer.cards.first
+    
     @chef = Chef.find(params[:format])
     @order = OrderReserver.new
   end
