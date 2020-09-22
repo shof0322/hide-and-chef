09b7303b05d2ff5a536b12d7d9d2372f59f68908
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-09-22 12:28:23 +0900

    決済機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
 
     customer = Payjp::Customer.retrieve(card.customer_token)
     @card = customer.cards.first
-    
+
     @chef = Chef.find(params[:format])
     @order = OrderReserver.new
   end
@@ -17,6 +17,13 @@ class OrdersController < ApplicationController
     @order = OrderReserver.new(order_params)
     if @order.valid?
       @order.save
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
+      customer_token = current_user.card.customer_token # ログインしているユーザーの顧客トークンを定義
+      Payjp::Charge.create(
+        amount: @chef.course.price, # 商品の値段
+        customer: customer_token, # 顧客のトークン
+        currency: 'jpy' # 通貨の種類（日本円）
+      )
       return redirect_to root_path
     else
       render "new"
