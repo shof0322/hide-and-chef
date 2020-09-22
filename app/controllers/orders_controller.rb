7eaffe72e3e6caf051f65b34dead12a106344e4a
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-09-22 12:12:22 +0900

    コース予約情報保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,23 @@
 class OrdersController < ApplicationController
   def new
     @chef = Chef.find(params[:format])
-    @order = Order.new
+    @order = OrderReserver.new
   end
 
   def create
-    @order = Order.create(order_params)
+    @chef = Chef.find(params[:format])
+    @order = OrderReserver.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:user_id, :course_id)
+    params.require(:order_reserver).permit(:user_id, :course_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_num, :reserve_date, :reserve_time).merge(user_id: current_user.id, course_id: @chef.course.id)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,4 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :course
   has_many :reservers
-
-  with_options presence: true do
-    validates :user_id, :course_id
-  end
 end
